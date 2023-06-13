c61fc0408cea407bd8accf61eca45f9612cee3f5
一色 <>
2023-06-13 20:10:54 +0900

    chapter5/cookie

--- chapter5/cookie/login.php
+++ chapter5/cookie/login.php
@@ -1,3 +1,16 @@
+<?php
+if (!empty($_POST['submit'])) {
+    if ($_POST['username'] && $_POST['password']) {
+        setcookie('name', $_POST['username'], time() + 180);
+        setcookie('pass', $_POST['password'], time() + 180);
+        header("Location: welcome.php");
+    } else {
+        setcookie('name', $_POST['username'], time() - 1800);
+        setcookie('pass', $_POST['password'], time() - 1800);
+        var_dump('クッキー破棄成功');
+    }
+}
+?>
 <!DOCTYPE html>
 <html>
 
@@ -8,11 +21,21 @@
 
 <body>
     <form action="login.php" method="post">
+        <?php if (!empty($_COOKIE['name']) && !empty($_COOKIE['pass'])) { ?>
+
+            <label>ユーザー名</label>
+            <input type="text" name="username" value="<?= $_COOKIE['name']; ?>"><br>
+            <label>パスワード</label>
+            <input type="text" name="password" value="<?= $_COOKIE['pass']; ?>"><br>
+
+        <?php } else { ?>
 
             <label>ユーザー名</label>
             <input type="text" name="username"><br>
             <label>パスワード</label>
             <input type="text" name="password"><br>
+
+        <?php } ?>
 
         <input type="submit" value="ログイン" name="submit">
     </form>
