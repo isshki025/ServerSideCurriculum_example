c2677ad3741cce25950ea1c0674bca195af25953
一色 <>
2023-06-13 14:46:49 +0900

    BASE10_re/work

--- PHP_BASE10_re/work/index.php
+++ PHP_BASE10_re/work/index.php
@@ -1,18 +1,29 @@
 <?php
-function toeic($point){
-    if($point < 400){
+function toeic($point)
+{
+    if ($point < 400) {
         $level = '英検3~5級程度';
-    }elseif($point >= 400 && $point < 500){
+    } else if ($point >= 400 && $point < 500) {
         $level = '英検準2級程度';
-    }
     //➀残りの条件も作ってあげよう
+    } else if ($point >= 500 && $point < 600) {
+        $level = '英検2級程度';
+    } else if ($point >= 600 && $point < 700) {
+        $level = '英検準2級Aレベル程度';
+    } else if ($point >= 700 && $point < 800) {
+        $level = '英検準2級A〜準1級レベル程度';
+    } else if ($point >= 800 && $point < 900) {
+        $level = '英検準1級程度';
+    } else {
+        $level = '英検1級程度';
+    }
 
     //➁戻り値で英検がどのレベルかを渡してあげるようにしよう。
-
+    return $level;
 }
 
 
 //➂toeic関数の結果が入る$text変数に定義しよう
-// $text = ;
-// echo "あなたの英語力は{$text}となります。";
+$text = toeic(900);
+echo "あなたの英語力は{$text}となります。";
 ?>
