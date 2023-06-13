ef059e0badfa523cba2e0064e1a1ef2bc0d1f7e5
一色 <>
2023-06-13 20:07:01 +0900

    chapter5/ajax

--- chapter5/ajax/index.js
+++ chapter5/ajax/index.js
@@ -1,4 +1,31 @@
-$(function() {
-  //この中に、全てのコードを書いていきます。
-  
+$(function () {
+  //この中に、すべてのコードを書いていきます。
+  $('select[name="question"]').change(function () {
+    var value = "";
+
+    value = $(this).val();
+    console.log(value);
+
+    $.ajax("int.php", {
+      type: "get",
+      data: { val: value },
+      datatype: "json"
+    })
+    .done(function (data) {
+      //ここにPHPからの返答があった場合の処理を書きます。
+      console.log(data);
+      $('select[name="answer"] > option').remove();
+      data.forEach(function (times) {
+        $('select[name="answer"]').append(
+          $("<option>")
+            .val(times)
+            .text(times)
+        );
+      });
+    })
+    .fail(function () {
+      //ここにPHPからの返答があった場合の処理を書きます。
+      console.log("PHPからのデータ受け取りが失敗しました");
+    });
+  });
 });
