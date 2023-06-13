23bc28585fc830f0c0931f6c79d4303a5b9939fc
一色 <>
2023-06-13 14:43:00 +0900

    BASE05/work

--- PHP_BASE05/work/index.php
+++ PHP_BASE05/work/index.php
@@ -2,11 +2,12 @@
 $name =  "Atlas太郎" ;
 
 // 下記の条件分岐内で、３つの分岐を完成してください
-if($name == ) {
-    echo ;
-}
-else if () {
-    echo ;
+if ($name == 'Atlas次郎') {
+    echo '名前がIFで一致した';
+} else if ($name == 'Atlas太郎') {
+    echo '名前がELSE IFで一致した';
+} else {
+    echo '名前が一致しなかった';
 }
 
 ?>
